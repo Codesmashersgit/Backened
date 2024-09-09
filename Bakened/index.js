8c1ef42c5e38dd1d657a68513fba2495bc0dd5a8
Sudhanshu Raj <sudhanshu.ok1802@gmail.com>
2024-09-10 01:32:05 +0530

    Changes Success

--- Bakened/index.js
+++ Bakened/index.js
@@ -1,13 +1,39 @@
 
-require('dotenv').config()
-const express= require('express');
-const app= express();
-const port=4000;
+const express = require('express');
 
-app.get('/',(req,res)=>{
-    res.send("kya be kya ho rha h");
-})
+const app = express();
 
-app.listen(process.env.PORT,()=>{
-    console.log(`succesfully compiled with port ${port}`)
-})
+app.get('/api/jokes', (req, res) => {
+    const jokes = [
+        {
+            id:1,
+            title: "First Joke",
+            content: "Ye mera First Backened Project hai",
+        },
+        {
+            id:2,
+            title: "Second Joke",
+            content: "Aur tmhara",
+        },
+        {
+            id:3,
+            title: "Third joke",
+            content: "Chori krke banaya hu dukh mtt ho itna",
+        },
+        {
+            id:4,
+            title: "Fourth joke",
+            content: "Khus ho jaa bsdk",
+        },
+        {
+            id:5,
+            title:"Fifth joke",
+            content:"Ek tha raja ek thi rani dono marr gyi khatam kahani bye bye😂",
+        }
+    ];
+    res.send(jokes); 
+});
+
+app.listen(3000, () => {
+    console.log(`Successfully running on port 3000`);
+});
